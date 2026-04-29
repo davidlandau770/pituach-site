41ca328a63b55c5a73f3bf9acc20eac64805b794
davidlandau770 <dal778770@gmail.com>
2026-04-29 14:31:36 +0300

    refactor: remove unnecessary middleware logging in router

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,10 @@ app.use((req, _, next) => {
   next();
 });
 app.use(express.json());
+router.use((req, _, next) => {
+  console.log("3");
+  next();
+});
 app.use(router);
 app.use(handleError);
 
--- server/src/middlewares/router.ts
+++ server/src/middlewares/router.ts
@@ -2,10 +2,7 @@ import express from "express";
 import contactRoutes from "../contact/routes/contact.routes";
 
 const router = express.Router();
-router.use((req, _, next) => {
-  console.log("3");
-  next();
-});
+
 router.use("/api/contact", contactRoutes);
 
 export default router;
