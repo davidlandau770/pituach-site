1bb2bfc607b4eacbac36aeef17696ef772d80e30
davidlandau770 <dal778770@gmail.com>
2026-04-29 12:58:18 +0300

    fix: ensure CORS_WHITE_LIST has a default value of an empty array

--- server/src/helpers/environments.ts
+++ server/src/helpers/environments.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || "9000";
-export const CORS_WHITE_LIST = process.env.CORS_WHITE_LIST;
+export const CORS_WHITE_LIST = process.env.CORS_WHITE_LIST || "[]";
 export const GOOGLE_SHEET_URL = process.env.GOOGLE_SHEET_URL || "";
