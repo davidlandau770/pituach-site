b96da255aacf17d7620135f3734e788db4d01f9b
davidlandau770 <dal778770@gmail.com>
2026-04-29 11:29:47 +0300

    change to vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import cors from "./middlewares/cors";
 import router from "./middlewares/router";
 import handleError from "./middlewares/handleServerError";
@@ -10,10 +9,6 @@ const app = express();
 app.use(cors);
 app.use(express.json());
 app.use(router);
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
 app.use(handleError);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
